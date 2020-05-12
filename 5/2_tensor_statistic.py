85962663a410b9d46c4377df62e7931ab154ec93
erande <1873594231@qq.com>
2020-05-12 18:04:43 +0800

    add file for tensor merge/split/statistic/sort and accuracy compute.

---
+++ 5/2_tensor_statistic.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+
+# vector norm: 向量范数
+a = tf.ones([2, 2])
+print(tf.norm(a))  # 二范数，所有元素平方和开根号
+print(tf.sqrt(tf.reduce_sum(tf.square(a))))  # 结果与上面方式相同
+
+# axis=0代表按列计算，axis=1代表按行计算
+print(tf.norm(a, ord=2, axis=1))  # ord=2代表二范数，axis代表指定维度的数据求范数
+print(tf.norm(a, ord=1))  # ord=1代表一范数，所有元素绝对值之和
+print(tf.norm(a, ord=1, axis=0))
+print(tf.norm(a, ord=1, axis=1))
+
+# reduce_mean/reduce_max/reduce_min/reduce_sum
+a = tf.random.normal([4, 10])
+print(tf.reduce_min(a))  # shape=()
+print(tf.reduce_max(a))  # shape=()
+print(tf.reduce_mean(a))  # shape=(), 求均值
+
+print(tf.reduce_min(a, axis=1))  # shape=(4,), 求每一行的最小值
+print(tf.reduce_max(a, axis=1))  # shape=(4,), 求每一行的最大值
+print(tf.reduce_mean(a, axis=1))  # shape=(4,), 求每一行的均值
+
+# argmax/argmin: 最大/小值的index
+print(tf.argmax(a))  # axis默认为0， shape=(10,)
+print(tf.argmax(a, axis=1))  # shape=(4,)
+
+# tf.equal
+a = tf.constant([1, 2, 3, 4, 5])
+b = tf.range(5)
+print(tf.equal(a, b))  # False, False, False, False, False
+res = tf.equal(a, b)
+print(tf.reduce_sum(tf.cast(res, dtype=tf.int32)))  # bool值转化为0，1数值再进行sum，可统计相同元素的个数
+
+a = tf.constant([[0.1, 0.2, 0.7], [0.9, 0.05, 0.05]])
+pred = tf.cast(tf.argmax(a, axis=1), dtype=tf.int32)
+y = tf.constant([2, 1])
+correct = tf.reduce_sum(tf.cast(tf.equal(y, pred), dtype=tf.int32)) / 2
+
+# tf.unique: 去除重复元素
+a = tf.constant([4, 2, 2, 4, 3])
+print(tf.unique(a))
+# shape=(3,), unique numpy=array([4, 2, 3]), index numpy=array([0, 1, 1, 0, 2])
+# 还原： tf.gather([4, 2, 3], [0, 1, 1, 0, 2])
+
---
+++ 5/3_tensor_sort.py
@@ -0,0 +1,52 @@
+import os
+import tensorflow as tf
+
+tf.enable_eager_execution()
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '1'  # 这是默认的显示等级，显示所有信息
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2'  # 只显示 warning 和 Error
+# os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'  # 只显示 Error
+
+# tf.sort/tf.argsort，默认最后一个维度升序排序
+a = tf.random.shuffle(tf.range(5))
+print(tf.sort(a, direction='DESCENDING'))  # numpy=array([4, 3, 2, 1, 0])
+print(tf.argsort(a, direction='DESCENDING'))  # 返回元素索引值
+idx = tf.argsort(a, direction='DESCENDING')
+print(tf.gather(a, idx))  # 根据索引可以通过tf.gather返回降序排序结果
+
+# topk
+res = tf.math.top_k(a, 2)
+print(res.indices)  # 返回最大的2个值的index
+print(res.values)  # 返回最大的2个值
+
+# top k accuracy
+prob = tf.constant([[0.1, 0.2, 0.7], [0.2, 0.7, 0.1]])
+# target = tf.broadcast_to(tf.constant([2, 0]), [3, 2])  # numpy=array([[2, 0], [2, 0], [2, 0]]
+# topk = tf.transpose(tf.math.top_k(prob, 3).indices, [1, 0])  # numpy=array([[2, 1], [1, 0], [0, 2]]
+# top1: [2, 0], [2, 1] => 1/2=50%
+# top2: [2, 0], [2, 1] => 1/2=50%  + [2, 0], [1, 0] => 1/2=50% = 100%
+# top2: [2, 0], [2, 1] => 1/2=50%  + [2, 0], [1, 0] => 1/2=50% + [2, 0], [0, 2] => 0/2=0 = 100%
+target = tf.constant([2, 0])
+
+
+def accuracy(output, y, topk=(1,)):
+    maxk = max(topk)  # 可以计算top1,top2,topk的准确率
+    # tf1.0: batch_size = tf.cast(tf.convert_to_tensor(y.shape[0]), dtype=tf.float32)
+    # tf1.0: pred = tf.nn.top_k(output, maxk).indices
+    batch_size = y.shape[0]
+    pred = tf.math.top_k(output, maxk).indices
+    pred = tf.transpose(pred, perm=[1, 0])
+    y_ = tf.broadcast_to(y, pred.shape)
+    correct = tf.equal(pred, y_)
+
+    res = []
+    for k in topk:
+        correct_k = tf.cast(tf.reshape(correct[:k], [-1]), dtype=tf.float32)
+        correct_k = tf.reduce_sum(correct_k)
+        acc = float(correct_k / batch_size)
+        res.append(acc)
+
+    return res
+
+
+res = accuracy(prob, target, topk=(1, 2, 3))
+print(res)  # [0.5, 1.0, 1.0], 与上述手动计算结果相同
